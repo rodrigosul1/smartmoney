38f3891da0c01833036e9b94c219bf7c5435a3c1
Rodrigo Sul <rodrigosul10@gmail.com>
2024-08-20 14:28:40 -0300

    Started: Auth

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher([
-  '/',
+const isProtectedRoute = 
+  createRouteMatcher([
+    '/',
 ])
 
 export default clerkMiddleware((auth, req) => {
